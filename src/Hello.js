524e097b16c91e010f3a9b3283587cd5a82276c4
Danny <danny.wang@autodesk.com>
2015-10-12 19:54:28 +0800

    init by danny

---
+++ src/Hello.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * Created by wangda on 10/12/2015.
+ */
+
+
+
+class Hello {
+    constructor(){
+        console.log("constructor of Hello here");
+        init();
+    }
+    init(){
+        console.log("init of hello here" );
+    }
+}
